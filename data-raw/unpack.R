6cd9998c566630dd3de87f3046d6c8fd1eb2f21f
jennybc <jenny@stat.ubc.ca>
2017-01-21 22:12:01 -0800

    Unpack them all!

---
+++ data-raw/unpack.R
@@ -0,0 +1,9 @@
+library(excelgesis)
+library(here)
+library(purrr)
+
+examples <- xg_example(".*")
+walk(examples,
+     ~ xg_unzip(.x,
+                exdir = here("inst", "extdata",
+                      tools::file_path_sans_ext(basename(.x)))))
