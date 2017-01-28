9f71633fcdb4cd16a78287270e29d5a3e2b3f443
jennybc <jenny@stat.ubc.ca>
2017-01-28 08:13:05 -0800

    Add empty sheets and sheets for skipping

--- R/xg_unzip.R
+++ R/xg_unzip.R
@@ -5,7 +5,8 @@
 #'
 #' @param path Character, length one. Path to xlsx file.
 #' @param exdir Passed to \code{tools::unzip}. Directory in which to unpack the
-#'   xlsx.
+#'   xlsx. By default, if unpacking \code{foo.xlxs}, this will be \code{foo}
+#'   (unlike the default for \code{tools::unzip}).
 #'
 #' @return The path to the top-level directory of the unpacked xlsx, invisibly.
 #' @export
@@ -16,7 +17,7 @@
 xg_unzip <- function(path, exdir = character()) {
   stopifnot(is.character(path), length(path) == 1L)
   if (length(exdir) < 1) {
-    exdir <- tools::file_path_sans_ext(basename(path))
+    exdir <- tools::file_path_sans_ext(path)
   }
   fl <- utils::unzip(path, exdir = exdir)
   message("Unpacked these files:\n", paste0(paste0("  * ", fl), collapse = "\n"))
@@ -24,8 +25,9 @@ xg_unzip <- function(path, exdir = character()) {
 }
 
 least_common_dir <- function(paths) {
-  parts <- stringr::str_split_fixed(paths, pattern = "/", n = 4)
+  n <- stringr::str_count(paths, "/") + 1
+  parts <- stringr::str_split_fixed(paths, pattern = "/", n = n)
   n_distinct <- apply(parts, 2, function(z) length(unique(z)))
   lcd_index <- max(which(n_distinct == 1))
-  parts[[1, lcd_index]]
+  do.call(file.path, as.list(parts[1, seq_len(lcd_index)]))
 }
