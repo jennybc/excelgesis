8d311e689185057099e1623f561c7e3ef5878f8c
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-03-21 20:14:39 -0700

    Revisit xg_unzip()

--- R/xg_unzip.R
+++ R/xg_unzip.R
@@ -14,20 +14,15 @@
 #' @examples
 #' (path <- xg_example("mini"))
 #' (x <- xg_unzip(path))
-xg_unzip <- function(path, exdir = character()) {
+xg_unzip <- function(path, exdir = NULL) {
   stopifnot(is.character(path), length(path) == 1L)
-  if (length(exdir) < 1) {
-    exdir <- tools::file_path_sans_ext(path)
+  if (is.null(exdir)) {
+    exdir <- fs::path_ext_remove(path)
   }
   fl <- utils::unzip(path, exdir = exdir)
-  message("Unpacked these files:\n", paste0(paste0("  * ", fl), collapse = "\n"))
-  invisible(least_common_dir(fl))
-}
-
-least_common_dir <- function(paths) {
-  n <- stringr::str_count(paths, "/") + 1
-  parts <- stringr::str_split_fixed(paths, pattern = "/", n = n)
-  n_distinct <- apply(parts, 2, function(z) length(unique(z)))
-  lcd_index <- max(which(n_distinct == 1))
-  do.call(file.path, as.list(parts[1, seq_len(lcd_index)]))
+  message(
+    "Unpacked these files:\n",
+    paste0("  * ", fl, collapse = "\n")
+  )
+  invisible(exdir)
 }
