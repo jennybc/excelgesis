acc4879b5eacb434f78f1c8723076fbacf775ec1
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-03-21 23:18:05 -0700

    Default to unzipping in working dir; bring back strip_xlsx

--- R/xg_unzip.R
+++ R/xg_unzip.R
@@ -5,19 +5,19 @@
 #'
 #' @param path Character, length one. Path to xlsx file.
 #' @param exdir Passed to \code{tools::unzip}. Directory in which to unpack the
-#'   xlsx. By default, if unpacking \code{foo.xlxs}, this will be \code{foo}
-#'   (unlike the default for \code{tools::unzip}).
+#'   xlsx. By default, if unpacking \code{foo.xlxs}, this will be \code{foo} in
+#'   current working directory.
 #'
 #' @return The path to the top-level directory of the unpacked xlsx, invisibly.
 #' @export
 #'
 #' @examples
-#' (path <- xg_example("mini"))
+#' (path <- xg_example("datasets"))
 #' (x <- xg_unzip(path))
 xg_unzip <- function(path, exdir = NULL) {
   stopifnot(is.character(path), length(path) == 1L)
   if (is.null(exdir)) {
-    exdir <- fs::path_ext_remove(path)
+    exdir <- fs::path_file(strip_xlsx(path))
   }
   fl <- utils::unzip(path, exdir = exdir)
   message(
@@ -25,4 +25,12 @@ xg_unzip <- function(path, exdir = NULL) {
     paste0("  * ", fl, collapse = "\n")
   )
   invisible(exdir)
+}
+
+strip_xlsx <- function(path) {
+  if (fs::path_ext("foo.xlsx") == "xlsx") {
+    fs::path_ext_remove(path)
+  } else {
+    path
+  }
 }
